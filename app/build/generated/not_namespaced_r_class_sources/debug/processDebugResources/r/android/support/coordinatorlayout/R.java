9682fd5516ceeb9db25da2f2e5e1300bc7315a81
VIVI-XX <395425496@qq.com>
2019-06-09 21:28:55 +0800

    解决问题：home页面图片成功显示

--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/coordinatorlayout/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/coordinatorlayout/R.java
@@ -100,28 +100,28 @@ public final class R {
         public static final int forever = 0x7f080057;
         public static final int icon = 0x7f080061;
         public static final int icon_group = 0x7f080062;
-        public static final int info = 0x7f080065;
-        public static final int italic = 0x7f080067;
-        public static final int left = 0x7f080071;
-        public static final int line1 = 0x7f080072;
-        public static final int line3 = 0x7f080073;
-        public static final int none = 0x7f080083;
-        public static final int normal = 0x7f080084;
-        public static final int notification_background = 0x7f080085;
-        public static final int notification_main_column = 0x7f080086;
-        public static final int notification_main_column_container = 0x7f080087;
-        public static final int right = 0x7f080096;
-        public static final int right_icon = 0x7f080097;
-        public static final int right_side = 0x7f080098;
-        public static final int start = 0x7f0800bf;
-        public static final int tag_transition_group = 0x7f0800c5;
-        public static final int tag_unhandled_key_event_manager = 0x7f0800c6;
-        public static final int tag_unhandled_key_listeners = 0x7f0800c7;
-        public static final int text = 0x7f0800c8;
-        public static final int text2 = 0x7f0800c9;
-        public static final int time = 0x7f0800d4;
-        public static final int title = 0x7f0800d5;
-        public static final int top = 0x7f0800d8;
+        public static final int info = 0x7f080066;
+        public static final int italic = 0x7f080068;
+        public static final int left = 0x7f080072;
+        public static final int line1 = 0x7f080073;
+        public static final int line3 = 0x7f080074;
+        public static final int none = 0x7f080084;
+        public static final int normal = 0x7f080085;
+        public static final int notification_background = 0x7f080086;
+        public static final int notification_main_column = 0x7f080087;
+        public static final int notification_main_column_container = 0x7f080088;
+        public static final int right = 0x7f080097;
+        public static final int right_icon = 0x7f080098;
+        public static final int right_side = 0x7f080099;
+        public static final int start = 0x7f0800c0;
+        public static final int tag_transition_group = 0x7f0800c6;
+        public static final int tag_unhandled_key_event_manager = 0x7f0800c7;
+        public static final int tag_unhandled_key_listeners = 0x7f0800c8;
+        public static final int text = 0x7f0800c9;
+        public static final int text2 = 0x7f0800ca;
+        public static final int time = 0x7f0800d5;
+        public static final int title = 0x7f0800d6;
+        public static final int top = 0x7f0800d9;
     }
     public static final class integer {
         private integer() {}
@@ -141,7 +141,7 @@ public final class R {
     public static final class string {
         private string() {}
 
-        public static final int status_bar_notification_info_overflow = 0x7f0d0052;
+        public static final int status_bar_notification_info_overflow = 0x7f0d0041;
     }
     public static final class style {
         private style() {}
