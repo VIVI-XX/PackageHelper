5bcd0d5f9e8adacf60207aa18cdd535e90153e35
VIVI-XX <395425496@qq.com>
2019-06-09 16:16:33 +0800

    登陆主页面设计。

--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
@@ -74,19 +74,19 @@ public final class R {
         private id() {}
 
         public static final int bottom = 0x7f080022;
-        public static final int end = 0x7f080043;
-        public static final int gone = 0x7f080055;
-        public static final int invisible = 0x7f08005f;
-        public static final int left = 0x7f08006a;
-        public static final int packed = 0x7f080081;
-        public static final int parent = 0x7f080083;
-        public static final int percent = 0x7f080086;
-        public static final int right = 0x7f08008e;
-        public static final int spread = 0x7f0800b1;
-        public static final int spread_inside = 0x7f0800b2;
-        public static final int start = 0x7f0800b7;
-        public static final int top = 0x7f0800cc;
-        public static final int wrap = 0x7f0800dc;
+        public static final int end = 0x7f080044;
+        public static final int gone = 0x7f080056;
+        public static final int invisible = 0x7f080060;
+        public static final int left = 0x7f08006b;
+        public static final int packed = 0x7f080082;
+        public static final int parent = 0x7f080084;
+        public static final int percent = 0x7f080087;
+        public static final int right = 0x7f08008f;
+        public static final int spread = 0x7f0800b2;
+        public static final int spread_inside = 0x7f0800b3;
+        public static final int start = 0x7f0800b8;
+        public static final int top = 0x7f0800cd;
+        public static final int wrap = 0x7f0800dd;
     }
     public static final class styleable {
         private styleable() {}
--- app/src/main/java/com/example/tinkpad/packagehelper/HomeActivity.java
+++ app/src/main/java/com/example/tinkpad/packagehelper/HomeActivity.java
@@ -1,13 +1,31 @@
 package com.example.tinkpad.packagehelper;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.Gravity;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends FragmentActivity {
+    private Fragment mFragments[];
+    private RadioGroup radioGroup;
+    private FragmentManager fragmentManager;
+    private FragmentTransaction fragmentTransaction;
+    private RadioButton rbtHome,rbtHelpMe,rbtHelpYou;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+
+
+
     }
 }
